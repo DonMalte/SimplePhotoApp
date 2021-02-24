48e3f6bcccb0b72c0b9ee29d8507c34203270a43
Malte Schoppe <malte.schoppe@gmail.com>
2021-02-24 17:30:32 +0100

    feat: show the photos of an album when selected

--- SimplePhotoApp/Controller/PhotoCollectionViewController.swift
+++ SimplePhotoApp/Controller/PhotoCollectionViewController.swift
@@ -11,63 +11,57 @@ private let photoCellReuseIdentifier = "photoCell"
 
 class PhotoCollectionViewController: UICollectionViewController {
 
+    var photos = [Photo]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        //The title of the navigationItem is set in the prepare(for segue) function in the previous AlbumCollectionViewController
         
-
     }
 
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+
+        return photos.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "", for: indexPath)
-    
-        // Configure the cell
-    
-        return cell
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: photoCellReuseIdentifier, for: indexPath) as? PhotoCell {
+            let photo = photos[indexPath.item]
+            
+            cell.photo = photo
+            
+            return cell
+        }
+        
+        return UICollectionViewCell()
     }
 
     // MARK: UICollectionViewDelegate
-
-    /*
-    // Uncomment this method to specify if the specified item should be highlighted during tracking
-    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
-        return true
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        let photo = photos[indexPath.item]
+        
+        
     }
-    */
-
-    /*
-    // Uncomment this method to specify if the specified item should be selected
-    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-    */
-
-    /*
-    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
-    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
-        return false
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
-    }
-    */
+}
 
+extension PhotoCollectionViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        //The default view is a photo that is half the screens width, with a spacing of 1 between cells and lines
+        let halfScreenWidth = collectionView.frame.width/2-1
+        let size = CGSize(width: halfScreenWidth, height: halfScreenWidth)
+        
+        return size
+    }
 }
--- SimplePhotoApp/View/Cells/PhotoCell.swift
+++ SimplePhotoApp/View/Cells/PhotoCell.swift
@@ -11,5 +11,18 @@ class PhotoCell: UICollectionViewCell {
     
     @IBOutlet weak var photoCellImageView: UIImageView!
     
+    ///Set the photo for it to be displayed in the cell
+    var photo: Photo? {
+        didSet {
+            if let photo = photo {
+                if let url = URL(string: photo.url) {
+                    photoCellImageView.sd_setImage(with: url, completed: nil)
+                }
+            }
+        }
+    }
     
+    override func prepareForReuse() {
+        photoCellImageView.image = nil
+    }
 }

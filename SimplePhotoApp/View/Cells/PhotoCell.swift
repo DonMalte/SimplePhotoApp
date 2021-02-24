94d851864530c9f24f29a87e905db8c81f5ba454
Malte Schoppe <malte.schoppe@gmail.com>
2021-02-24 16:44:17 +0100

    add: photoCollectionView and photoCell

---
+++ SimplePhotoApp/View/Cells/PhotoCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoCell.swift
+//  SimplePhotoApp
+//
+//  Created by Malte Schoppe on 24.02.21.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoCellImageView: UIImageView!
+    
+    
+}

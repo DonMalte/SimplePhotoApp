b4396b415b7117b654da58d58bfcbc00cb965659
Malte Schoppe <malte.schoppe@gmail.com>
2021-02-24 18:35:21 +0100

    add: DetailViewController

---
+++ SimplePhotoApp/Controller/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  SimplePhotoApp
+//
+//  Created by Malte Schoppe on 24.02.21.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailViewTitleLabel: UILabel!
+    @IBOutlet weak var detailViewImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}

d1161b12603d1e1ee249654deb8f9c2461ea6f64
Malte Schoppe <malte.schoppe@gmail.com>
2021-02-24 19:08:39 +0100

    feat: show full size photo incl. title when photo is selected

--- SimplePhotoApp/Controller/DetailViewController.swift
+++ SimplePhotoApp/Controller/DetailViewController.swift
@@ -12,9 +12,25 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var detailViewTitleLabel: UILabel!
     @IBOutlet weak var detailViewImageView: UIImageView!
     
+    var photo: Photo?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        showPhoto()
+    }
+    
+    //Show photo and title of the photo object
+    private func showPhoto() {
+        guard let photo = photo else {
+            navigationController?.popViewController(animated: true)
+            return
+        }
+        
+        if let url = URL(string: photo.url) {
+            detailViewImageView.sd_setImage(with: url, completed: nil)
+        }
+        detailViewTitleLabel.text = photo.title
     }
 
 }
